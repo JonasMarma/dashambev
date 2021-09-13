cb4b74fe9f8747e59239dd463a8414ef13acfe6d
Paulo Hoffmann <paulo.hoffmann@comuniki.me>
2021-09-13 16:07:56 -0300

    fix: finishing Envios por dia

--- src/pages/Envios/index.js
+++ src/pages/Envios/index.js
@@ -1,100 +1,74 @@
 import React from "react";
-import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
+import { Button, Card, Col, Form, InputGroup, Row } from "react-bootstrap";
 import Select from "react-select";
 import Layout from "../../components/Layout";
+import AmBarChart from "./amBarChart";
 import "./styles.scss";
 
 export const Envio = () => {
-  const productList = [
-    { value: "prod1", label: "Produto 1" },
-    { value: "prod2", label: "Produto 2" },
-    { value: "prod3", label: "Produto 3" },
+  const paramNameList = [
+    { value: "param1", label: "Param1" },
+    { value: "param2", label: "Param2" },
+    { value: "param3", label: "Param3" },
   ];
-  const ufList = [
-    { value: "prod1", label: "Produto 1" },
-    { value: "prod2", label: "Produto 2" },
-    { value: "prod3", label: "Produto 3" },
+  const dataChart = [
+    {
+      day: "01/09/2021",
+      sentPerDay: 65000,
+    },
+    {
+      day: "02/09/2021",
+      sentPerDay: 92000,
+    },
+    {
+      day: "03/09/2021",
+      sentPerDay: 88000,
+    },
+    {
+      day: "04/09/2021",
+      sentPerDay: 93000,
+    },
+    {
+      day: "05/09/2021",
+      sentPerDay: 90000,
+    },
+    {
+      day: "06/09/2021",
+      sentPerDay: 95000,
+    },
   ];
 
   return (
     <Layout>
       <Row>
-        <h1>Parametrização de régua de cobrança</h1>
+        <h1>Envios por dia</h1>
       </Row>
       <Row className="mb-4 mt-2">
-        <Col md={11} className="d-flex justify-content-between">
-          <Col style={{ marginRight: "10px" }}>
-            <p style={{ margin: "0", fontWeight: "bold" }}>Faixa de atraso</p>
-            <div className="d-flex justify-content-between">
-              <div style={{ marginRight: "10px" }}>
-                <p style={{ margin: "0" }}>Data Início</p>
-                <Form.Control type="date" name="date-begin" className="mr-1" />
-              </div>
-              <div>
-                <p style={{ margin: "0" }}>Data Final</p>
-                <Form.Control type="date" name="date-end" />
-              </div>
-            </div>
-          </Col>
-          <Col
-            style={{
-              alignSelf: "self-end",
-              marginRight: "10px",
-            }}
-          >
-            <p style={{ margin: "0", fontWeight: "bold" }}>Produto</p>
-            <div>
-              <Select options={productList} />
-            </div>
-          </Col>
-
-          <Col
-            style={{
-              alignSelf: "self-end",
-              marginRight: "10px",
-            }}
-          >
-            <span style={{ margin: "0", fontWeight: "bold" }}>
-              Data de Direcionamento
-            </span>
-
-            <div>
-              <Form.Control type="date" name="date-direc" className="mr-1" />
-            </div>
-          </Col>
-          <Col
-            style={{
-              alignSelf: "self-end",
-              marginRight: "30px",
-            }}
-          >
-            <p style={{ margin: "0", fontWeight: "bold" }}>UF</p>
-            <div
-              style={{
-                alignSelf: "self-end",
-              }}
-            >
-              <Select options={ufList} />
-            </div>
-          </Col>
-          <Col
-            md={1}
-            style={{
-              alignSelf: "self-end",
-            }}
-          >
-            <div
-              style={{
-                alignSelf: "self-end",
-              }}
-            >
-              <Button> Buscar </Button>
-            </div>
-          </Col>
-        </Col>
+        <p style={{ margin: "0", fontWeight: "bold" }}>Parâmetros criados</p>
+        <div
+          style={{
+            alignSelf: "self-end",
+          }}
+        >
+          <Select
+            placeholder={<div>Selecione a regra criada</div>}
+            options={paramNameList}
+          />
+        </div>
       </Row>
       <Row>
-        <Col md={12}>1</Col>
+        <Col>
+          <Card className=" p-3">
+            <header>
+              <h5>Envios por dia</h5>
+              <div>
+                <p className="headerValue">523.000</p>
+                <p className="headerValueLabel">Envios totais</p>
+              </div>
+            </header>
+            <AmBarChart data={dataChart} chartDiv="sentPerDay" />
+          </Card>
+        </Col>
       </Row>
     </Layout>
   );
